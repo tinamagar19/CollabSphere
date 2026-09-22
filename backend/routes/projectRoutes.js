127fa2f9bcef65f7ed8e1869c5113bf9a397958f
Tina Magar <tinathapamagar19@gmail.com>
2026-09-22 23:58:02 +0545

    Set up project routes

---
+++ backend/routes/projectRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { createProject, getProjects } = require('../controllers/projectController');
+
+router.post('/', createProject);
+router.get('/', getProjects);
+
+module.exports = router;
